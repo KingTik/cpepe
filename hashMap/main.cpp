4003263016918712c93a038acc649b9f4e61e239
tomek <tik.wesolowski@wp.pl>
2017-11-19 20:57:37 +0100

    usage of map

--- hashMap/main.cpp
+++ hashMap/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <map>
 
 
 
 int main(){
 
+std::map<int, std::string> month;
 
+month[1] = "january";
+month[2] = "february";
+
+
+std::cout<<"month 1: " << month[0] << '\n';
+
+
+
+	
+int var;
 
 }

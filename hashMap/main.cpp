6817c7c2c94f60283d75e24498090e8f0d9861cc
tomek <tik.wesolowski@wp.pl>
2017-11-20 21:09:43 +0100

    Added hash and custom sorting

--- hashMap/main.cpp
+++ hashMap/main.cpp
@@ -2,20 +2,36 @@
 #include <map>
 
 
+struct customHash{
+    std::int64_t _value;
+    customHash(std::string str){
+        _value = std::hash<std::string>{}(str);
+    }
+
+};
+
+struct customHashCmp{
+    bool operator()(const customHash& one, const customHash& two){
+        if( one._value > two._value){
+            return true; 
+        }
+        return false;
+        
+    }
+};
+
 
 int main(){
 
-std::map<int, std::string> month;
+    std::map<customHash, std::string, customHashCmp> month;
 
-month[1] = "january";
-month[2] = "february";
-
-
-std::cout<<"month 1: " << month[0] << '\n';
+    // int hash = std::hash<std::string> {}("a");
+    customHash a("pepe");
+    month[a] = "pipi";
 
 
+    std::cout << "-> " << month[a] << "\n";
 
-	
-int var;
+
 
 }

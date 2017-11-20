04add51c2651b442e10a362407455ef0a3d93f11
tomek <tik.wesolowski@wp.pl>
2017-11-20 21:57:32 +0100

    template hash constructor

--- hashMap/main.cpp
+++ hashMap/main.cpp
@@ -4,12 +4,14 @@
 
 struct customHash{
     std::int64_t _value;
-    customHash(std::string str){
-        _value = std::hash<std::string>{}(str);
+    template<typename T>
+    customHash(T&& data){
+        _value = std::hash<typename std::decay<T>::type>{}(data);
     }
 
 };
 
+//funktor sortujacy hash mape
 struct customHashCmp{
     bool operator()(const customHash& one, const customHash& two){
         if( one._value > two._value){
@@ -26,11 +28,11 @@ int main(){
     std::map<customHash, std::string, customHashCmp> month;
 
     // int hash = std::hash<std::string> {}("a");
-    customHash a("pepe");
-    month[a] = "pipi";
+    //customHash a("pepe");
+    month[customHash("pepe")] = "pipi";
 
 
-    std::cout << "-> " << month[a] << "\n";
+    std::cout << "-> " << month[customHash("pepe")] << "\n";
 
 
 
